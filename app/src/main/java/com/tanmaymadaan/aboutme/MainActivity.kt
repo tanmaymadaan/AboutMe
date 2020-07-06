116088086ae0fabc21d975652d38ea664ed39f9a
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-06 23:11:07 +0530

    Added addNickname function

--- app/src/main/java/com/tanmaymadaan/aboutme/MainActivity.kt
+++ app/src/main/java/com/tanmaymadaan/aboutme/MainActivity.kt
@@ -2,10 +2,23 @@ package com.tanmaymadaan.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
     }
 }
