4e36d1fc36cc7c417e42ec222f38fa299316a79b
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-06 23:32:12 +0530

    Adding clickHandler

--- app/src/main/java/com/tanmaymadaan/aboutme/MainActivity.kt
+++ app/src/main/java/com/tanmaymadaan/aboutme/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
     }
 
     private fun addNickname(view: View) {
