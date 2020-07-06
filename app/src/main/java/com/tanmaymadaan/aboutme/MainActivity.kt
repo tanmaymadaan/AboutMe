a72f4b9c7e39d9c0b9b0f2f19ebfef20ccab331d
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-06 23:12:12 +0530

    Hide keyboard

--- app/src/main/java/com/tanmaymadaan/aboutme/MainActivity.kt
+++ app/src/main/java/com/tanmaymadaan/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.tanmaymadaan.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 
@@ -20,5 +22,9 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nickNameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0);
     }
 }
